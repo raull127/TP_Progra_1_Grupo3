37d366adc6cef2b1a65535fe206f25a246bd10da
Dam234 <scaladamian25@gmail.com>
2025-08-28 12:03:36 -0300

    Add files via upload

---
+++ Programa/Programa.py
@@ -0,0 +1,13 @@
+
+
+usuarios = [[id, nombre, clave, saldo, moneda]
+            [id, nombre, pass, saldo, moneda]
+            [id, nombre, pass, saldo, moneda]...]
+
+transacciones = [[fecha, id_origen, id_destino, tipo, monto, moneda, detalle]
+                 [fecha, id_origen, id_destino, tipo, monto, moneda, detalle]
+                 [fecha, id_origen, id_destino, tipo, monto, moneda, detalle]...]
+
+contactos = [[id_owner, id_contacto, alias]
+             [id_owner, id_contacto, alias]
+             [id_owner, id_contacto, alias]...]
